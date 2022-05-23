c9a7e937a38b2798fc8280e2776808e00f05125a
Jason <>
2022-05-23 16:51:09 +0800

    ubuntu upgrade to honister

--- scripts/ubuntu-env-evk.sh
+++ scripts/ubuntu-env-evk.sh
@@ -4,11 +4,11 @@
 
 DOCKER_IMAGE_TAG="imx-yocto"
 DOCKER_WORKDIR="/opt/yocto"
-IMX_RELEASE="imx-5.10.72-2.2.0_desktop"
+IMX_RELEASE="imx-5.15.5-1.0.0_desktop"
 YOCTO_DIR="${DOCKER_WORKDIR}/${IMX_RELEASE}-build"
 MACHINE="imx8mqevk"
 DISTRO="imx-desktop-xwayland"
 IMAGES="imx-image-desktop"
 REMOTE="https://source.codeaurora.org/external/imx/imx-manifest"
-BRANCH="imx-linux-hardknott"
+BRANCH="imx-linux-honister"
 MANIFEST=${IMX_RELEASE}".xml"
